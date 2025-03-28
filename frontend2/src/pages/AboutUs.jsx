aa071d3d9460e7a468bcfba8d4be44dd66227fc7
sourishgrover <sourishgrover50@gmail.com>
2025-03-29 00:04:37 +0530

    add picture to aboout page

--- frontend2/src/pages/AboutUs.jsx
+++ frontend2/src/pages/AboutUs.jsx
@@ -298,7 +298,7 @@ const AboutPage = () => {
                   {/* Image placeholder */}
                   <div className="w-full h-full flex items-center justify-center">
                     <div className="w-full h-full flex items-center justify-center ">
-                    <img src={productImage} alt="Product" className="w-90 h-100" />
+                    <img src={productImage} alt="Product" className="w-100 h-120" />
                     </div>
                   </div>
                 </div>
